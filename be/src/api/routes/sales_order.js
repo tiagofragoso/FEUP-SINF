c33cec505a7594f062b83a5653147cf0369428ee
Rui Alves <up201606746@fe.up.pt>
2019-11-26 22:18:21 +0000

    Add endpoint to get all picked items in a sales order

--- be/src/api/routes/sales_order.js
+++ be/src/api/routes/sales_order.js
@@ -1,13 +1,30 @@
 const { Router } = require("express");
 const router = Router();
+const item = require("../../models/item");
 
 module.exports = (app) => {
     app.use("/sales-order", router);
 
     /**
-     * Gets all the picking waves
+     * Gets picked items for a sales order
+     * @param {String} id Sales Order id
+     * @returns {200} Success
+     * @returns {500} Internal Error
      */
-    router.get("/", (req, res) => {
-        res.send("hello sales order!");
+    router.get("/:id/picked-items", (req, res) => {
+        const { id } = req.params;
+
+        item.findAll({
+            where: {
+                sales_order: id,
+            },
+        })
+            .then((query_res) => {
+                const items = query_res.map(({ dataValues }) => dataValues);
+                res.json(items);
+            })
+            .catch(() => {
+                res.status(500).send("Failure");
+            });
     });
 };
