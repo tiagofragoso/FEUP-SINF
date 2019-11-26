44c4afbbed164214e1d1e922bff9ac22608b9160
Rui Alves <up201606746@fe.up.pt>
2019-11-26 19:20:34 +0000

    Create database schema

---
+++ be/src/db/schema.sql
@@ -0,0 +1,23 @@
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS picking_waves;
+CREATE TABLE picking_waves (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    due_date DATE NOT NULL,
+    is_done BOOLEAN NOT NULL DEFAULT false
+);
+
+DROP TABLE IF EXISTS items;
+CREATE TABLE items (
+    item_key TEXT PRIMARY KEY,
+    picking_wave REFERENCES picking_waves (id) NOT NULL,
+    sales_order TEXT NOT NULL,
+    name TEXT NOT NULL,
+    warehouse TEXT NOT NULL,
+    quantity INT NOT NULL CHECK (quantity >= 0) DEFAULT 0
+);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
