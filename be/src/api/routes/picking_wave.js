9917f30abc38f16ba6eb1834e818a5e0d9b08525
Rui Alves <up201606746@fe.up.pt>
2019-11-26 21:07:43 +0000

    Update picking wave routes to use db models

--- be/src/api/routes/picking_wave.js
+++ be/src/api/routes/picking_wave.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const router = Router();
+const picking_wave = require("../../models/picking_wave");
+const item = require("../../models/item");
 
 module.exports = (app) => {
     app.use("/picking-wave", router);
@@ -7,7 +9,46 @@ module.exports = (app) => {
     /**
      * Gets all the picking waves
      */
-    router.get("/", (req, res) => {
-        res.send("hello picking wave!");
+    router.get("/", (_, res) => {
+        picking_wave.findAll({
+            attributes: ["name", "due_date", "is_done"],
+        })
+            .then((query_res) => {
+                const picking_waves = query_res.map(({ dataValues }) => dataValues);
+                res.json(picking_waves);
+            })
+            .catch(() => {
+                res.status(500);
+                res.send("Failure");
+            });
+    });
+
+    /**
+     * Gets all items in a picking wave
+     * @param {Integer} id Picking wave id
+     */
+    router.get("/:id", (req, res) => {
+        const { id } = req.params;
+
+        if (isNaN(id)) {
+            res.status(400);
+            res.send("The picking order id must be an Integer");
+            return;
+        }
+
+        item.findAll({
+            attributes: ["item_key", "picking_wave", "sales_order", "name", "warehouse", "quantity"],
+            where: {
+                picking_wave: parseInt(id, 10),
+            },
+        })
+            .then((query_res) => {
+                const items = query_res.map(({ dataValues }) => dataValues);
+                res.json(items);
+            })
+            .catch(() => {
+                res.status(500);
+                res.send("Failure");
+            });
     });
 };
