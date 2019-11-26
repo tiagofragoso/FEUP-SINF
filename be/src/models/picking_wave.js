aca541da65137c5520a9d0d1bdc1aecd4025fd7e
Rui Alves <up201606746@fe.up.pt>
2019-11-26 20:53:21 +0000

    Add db models

---
+++ be/src/models/picking_wave.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const db = require("../db/db");
+
+const picking_wave = db.define("picking_waves", {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    due_date: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    is_done: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+});
+
+module.exports = picking_wave;
