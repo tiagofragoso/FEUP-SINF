650116c44aa4863acf8af75085cc117725fed7eb
Rui Alves <up201606746@fe.up.pt>
2019-12-15 10:50:11 +0000

    Add picking wave completion percentage to picking waves page

--- fe/src/pages/PickingWavesPage.js
+++ fe/src/pages/PickingWavesPage.js
@@ -11,6 +11,8 @@ const { TabPane } = Tabs;
 
 const IdWithLink = (id) => <Link to={id.toString()}>{id}</Link>
 
+const completionPercentage = (percentage) => percentage ? `${percentage}%` : "-";
+
 const table_columns = [
     {
         title: "ID",
@@ -27,6 +29,12 @@ const table_columns = [
         title: "Name",
         dataIndex: "name",
         key: "name",
+    },
+    {
+        title: "Completion Percentage",
+        dataIndex: "completion_percentage",
+        key: "completion_percentage",
+        render: completionPercentage,
     },
 ];
 
