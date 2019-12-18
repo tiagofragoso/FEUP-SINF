5ed2e3a82051a97f8ffa83b11bd01a2f6e60073d
Rui Alves <up201606746@fe.up.pt>
2019-12-18 08:26:02 +0000

    Add picking waves to populate script

--- be/src/db/populate.sql
+++ be/src/db/populate.sql
@@ -10,3 +10,24 @@ insert into warehouse_zones(id, x, y) values("WOODWINDS", 2, 2);
 insert into warehouse_zones(id, x, y) values("KEYBOARDS", 4, 2);
 insert into warehouse_zones(id, x, y) values("OTHER", 6, 2);
 insert into warehouse_zones(id, x, y) values("EXIT", 8, 1);
+
+-------------------
+-- Picking Waves --
+-------------------
+
+insert into picking_waves(name, due_date, is_done) values("PickingWave01", "22-12-2019", "true");
+insert into picking_waves(name, due_date, is_done) values("PickingWave02", "23-12-2019", "true");
+
+-----------
+-- Items --
+-----------
+
+insert into items(item_key, picking_wave_id, sales_order, name, quantity, is_picked) values("TAMBOURINE", 1, "ECL.2019.9", "Tambourine", 3, "true");
+insert into items(item_key, picking_wave_id, sales_order, name, quantity, is_picked) values("PIANO", 1, "ECL.2019.11", "Piano", 1, "true");
+insert into items(item_key, picking_wave_id, sales_order, name, quantity, is_picked) values("MODERN_OBOE", 1, "ECL.2019.3", "Modern Oboe", 1, "true");
+
+insert into items(item_key, picking_wave_id, sales_order, name, quantity, is_picked) values("TAMBOURINE", 2, "ECL.2019.9", "Tambourine", 2, "true");
+insert into items(item_key, picking_wave_id, sales_order, name, quantity, is_picked) values("PT_GUITAR", 2, "ECL.2019.4", "Portuguese Guitar", 3, "true");
+
+
+
